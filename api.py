87cafaf47dc58218f43c861ce9e5ffced68474f2
BeldenOng <109718519+BeldenOng@users.noreply.github.com>
2022-08-01 09:53:45 +0800

    1. Api

--- api.py
+++ api.py
@@ -1,23 +1,24 @@
 import requests
 
-api_key = "JU5Q2GN41LOPWZ76"
+def api():
+    api_key = "JU5Q2GN41LOPWZ76"
 
-url= f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=({api_key}'
+    url= f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=({api_key}'
 
-response = requests.get(url)
+    response = requests.get(url)
 
-data = response.json()
+    data = response.json()
 
-print(data.keys())
-
-
-
-pmdata = (data["Realtime Currency Exchange Rate"])
+    print(data.keys())
 
 
 
-CONVERSION = "[REAL TIME CURRENCY CONVERSION RATE]"
+    pmdata = (data["Realtime Currency Exchange Rate"])
 
-exchange_rate = float(pmdata['5. Exchange Rate'])
 
-final_exchange_rate = (f"{CONVERSION} USD1 = SGD{round((exchange_rate),6)}")
+
+    CONVERSION = "[REAL TIME CURRENCY CONVERSION RATE]"
+
+    exchange_rate = float(pmdata['5. Exchange Rate'])
+
+    final_exchange_rate = (f"{CONVERSION} USD1 = SGD{round((exchange_rate),6)}")
