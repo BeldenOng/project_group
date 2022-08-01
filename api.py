a14966292ad56481bfd2e137b8b0ddbfafb55d98
BeldenOng <109718519+BeldenOng@users.noreply.github.com>
2022-08-01 09:47:27 +0800

    test

---
+++ api.py
@@ -0,0 +1,23 @@
+import requests
+
+api_key = "JU5Q2GN41LOPWZ76"
+
+url= f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=({api_key}'
+
+response = requests.get(url)
+
+data = response.json()
+
+print(data.keys())
+
+
+
+pmdata = (data["Realtime Currency Exchange Rate"])
+
+
+
+CONVERSION = "[REAL TIME CURRENCY CONVERSION RATE]"
+
+exchange_rate = float(pmdata['5. Exchange Rate'])
+
+final_exchange_rate = (f"{CONVERSION} USD1 = SGD{round((exchange_rate),6)}")
---
+++ main.py
@@ -0,0 +1,3 @@
+git config --global user.name “BeldenOng”
+git config --global user.email “s10239988@connect.np.edu.sg”
+git config --global --list
