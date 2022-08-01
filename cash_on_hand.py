72ee51c425146df3de3eacdaa97308750a92974c
valeriejingxn <s10242412@connect.np.sg>
2022-08-01 22:29:35 +0800

    higher or lower

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-import re, csv 
+import re , csv 
 filepath_COH = Path.cwd()/"csv.reports"/"cash-on-hand-usd.csv"
 
 
 
 with filepath_COH.open(mode="r", encoding="UTF-8", newline = '') as COH_file:
         
-        reader = csv.reader(COH_file, delimiter = ',')
+        reader = csv.reader(COH_file)
         next(reader)
         list = []
         for line in reader:
@@ -16,16 +16,14 @@ with filepath_COH.open(mode="r", encoding="UTF-8", newline = '') as COH_file:
 
 def cashonhand():
      
-     for i in range (len(list)):
+    for i in range(0,len(list)):
 
         difference = int(list[i+1]) - int(list[i])
         if difference > 0 :
-            
             return("higher")
         elif difference < 0:
-            
             return("lower")
-             
+
 print(cashonhand())
 
        
