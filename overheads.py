2032eeb9cfe420c907e48d591dc53621571a4a6b
valeriejingxn <s10242412@connect.np.sg>
2022-08-02 21:06:24 +0800

    comments?

--- overheads.py
+++ overheads.py
@@ -1,19 +1,25 @@
+#import Path , re , csv
 from pathlib import Path
 import re , csv 
+#create variable for file path 
 filepath_overheads= Path.cwd()/"csv.reports"/"overheads-day-45.csv"
 
+#open overheads csv file using .open()
 with filepath_overheads.open(mode="r", encoding="UTF-8", newline = '') as overH_file:
+
+    #create variable reader 
     reader = csv.reader(overH_file)
-    
-    reader = csv.reader(overH_file)
+    #use next() to skip over header
     next(reader)
+    #create empty list
     list = []
 
+    #create for loop to iterate over the days 
     for line in reader:
+        #append values to empty list
         list.append(line)
         
-    print(sorted(list, key = lambda x: x[1], reverse=True)[0])  
-    
+    print(sorted(list, key = lambda x: x[1], reverse=True)[0])      
 
 
    
