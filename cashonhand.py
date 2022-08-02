0d0ddddc9b22356c28f17d93a1e1111a4bbeeee1
valeriejingxn <s10242412@connect.np.sg>
2022-08-02 21:03:51 +0800

    comments

--- cashonhand.py
+++ cashonhand.py
@@ -1,25 +1,36 @@
+# import Path , re , csv
 from pathlib import Path
 import re , csv 
+
+#create variable filepath_COH 
 filepath_COH = Path.cwd()/"csv.reports"/"cash-on-hand-usd.csv"
 
 
-
+#open csv file using .open()
 with filepath_COH.open(mode="r", encoding="UTF-8", newline = '') as COH_file:
-        
+        #reader variable reader
         reader = csv.reader(COH_file)
+        #use next() to skip over the headers
         next(reader)
+        #create empty list
         amount = []
         day = []
+        #create for loop 
         for line in reader:
+            #append cash on hand numbers to empty list
             amount.append(line[1])
+            #append the respective days to empty list
             day.append(line[0])
             
-
+#define cashonhand()
 def cashonhand():
+    #create for loop to iterate over all the days 
     for i in range(len(amount)-1):
-
+        #calculate the difference in Cash on hand amounts
         difference = int(amount[i+1]) - int(amount[i])
+        #creating condition when there is a cash deficit
         if difference < 0 :
             print(f"[CASH DEFICIT] Day: {round(float(day[i+1]), 2)}, AMOUNT: SGD{amount[i+1]}]")
-             
+        
+#call back the function             
 cashonhand()
